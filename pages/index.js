88b63816c8fa13b0542ff45c626d52f6bf5edbbc
Tuval Simha <tuval.simha@gmail.com>
2021-07-21 09:56:28 +0300

    New test

--- pages/index.js
+++ pages/index.js
@@ -10,7 +10,7 @@ export default function Home() {
 
       <main>
         <h1 className="title">
-          Tulikkkk <a href="https://nextjs.org">Next.js!</a>
+          Tulik <a href="https://nextjs.org">Next.js!</a>
         </h1>
 
         <p className="description">
