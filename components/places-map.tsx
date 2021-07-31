f5b64db039b4bfadfffb8673b895cb4b74f59f1d
Dotan Simha <dotansimha@gmail.com>
2021-07-31 17:14:24 +0300

    added markers

--- components/places-map.tsx
+++ components/places-map.tsx
@@ -1,12 +1,16 @@
 import React from "react";
-import ReactMapboxGl, { Layer, Feature } from "react-mapbox-gl";
+import ReactMapboxGl, { Marker } from "react-mapbox-gl";
 import "mapbox-gl/dist/mapbox-gl.css";
+import { GiTruck } from "react-icons/gi";
 
 // https://alex3165.github.io/react-mapbox-gl/demos
 const Map = ReactMapboxGl({
   accessToken:
     "pk.eyJ1IjoiZG90YW5zaW1oYSIsImEiOiJja3Jyc2F1Ym0xNnBmMm5td3drZWxjamltIn0.RQCUX6Eq7qP72EsQH6_WTg",
 });
+
+const icon = new Image(50, 50);
+icon.src = "/assets/icon-marker.png";
 
 export default function PlacesMap() {
   return (
@@ -19,9 +23,12 @@ export default function PlacesMap() {
         width: "100%",
       }}
     >
-      <Layer type="symbol" id="marker" layout={{ "icon-image": "marker-15" }}>
-        <Feature coordinates={[34.84455730231234, 32.06210893248879]} />
-      </Layer>
+      <Marker coordinates={[34.84455730231234, 32.06210893248879]}>
+        <GiTruck size={40} color="red" />
+      </Marker>
+      <Marker coordinates={[34.994557301312, 32.16110193247879]}>
+        <GiTruck size={40} color="blue" />
+      </Marker>
     </Map>
   );
 }
